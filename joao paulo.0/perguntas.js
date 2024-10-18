cb69f100a6f8cd2c99fea54daf22dd292fb5c82e
joaopaulo0786897 <162650657+joaopaulo0786897@users.noreply.github.com>
2024-10-17 22:36:52 -0300

    Add files via upload

---
+++ joao paulo.0/perguntas.js
@@ -0,0 +1,49 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+
+criaCartao(
+    'ciências',
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Entre 4 a 6 litros. São retirados 450 mililitros'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
+
+criaCartao(
+    'matematica',
+    'Quantas casas decimais tem o número pi?',
+    'infinitas'
+)
+
